5e9116be50ef6629013b07e329c73cb92b25d4ab
netanel tesfay <netaneltes66@gmailgit config --global user.email you@example.com.com>
2022-08-19 13:40:23 +0300

    1

---
+++ src/footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./footer.css"
+
+
+function Footer() {
+  return (
+    <div className="main-footer">
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h3 className="deatailsHead" >contect me</h3>
+                    <ul className="list">
+                        <li>Github</li>
+                        <li>Linkden</li>
+                        <li>Email</li>
+                        <li>Phone</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
